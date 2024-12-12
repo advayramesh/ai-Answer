2a3b2717dbf5e0fa8defbfcc4a7411aa3d4dcd60
advayramesh <advay.ramesh@gmail.com>
2024-12-12 02:59:02 -0500

    Fixed css issues

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="node" />
+/// <reference types="cheerio" />
+
+declare module 'cheerio' {
+    export * from 'cheerio';
+  }
+  
+  declare module 'node' {
+    export * from 'node';
+  }
